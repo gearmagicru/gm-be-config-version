67d2d809cf99db5e9e9fbe8a19715ebddf2eb327
Anton Tivonenko <93121899+gearmagicru@users.noreply.github.com>
2025-02-17 14:49:55 +0300

    Файлы локализации

---
+++ lang/text-en_GB.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Этот файл является частью расширения модуля веб-приложения GearMagic.
+ * 
+ * Пакет русской локализации.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    '{name}'       => 'Version',
+    '{description}' => 'System Version Information',
+    '{permissions}' => [
+        'any'  => ['Full access', 'View system version information']
+    ],
+
+    // Page
+    'System version information' => 'System version information',
+    'Web application' => 'Web application',
+    'Short name' => 'Short name',
+    'Control panel' => 'Control panel',
+    'Platform' => 'Platform',
+    'Version' => 'Version',
+    'Release date' => 'Release date',
+    'Details' => 'Details',
+    'Edition' => 'Edition',
+    'Administration panel' => 'Administration panel',
+    'Framework' => 'Framework',
+    'Update packages' => 'Update packages',
+    'Number of packages installed' => 'Number of packages installed',
+    'View service packs' => 'View service packs'
+];
---
+++ lang/text-ru_RU.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Этот файл является частью расширения модуля веб-приложения GearMagic.
+ * 
+ * Пакет русской локализации.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    '{name}'        => 'Версия',
+    '{description}' => 'Информация о версии системы',
+    '{permissions}' => [
+        'any'  => ['Полный доступ', 'Просмотр информация о версии системы']
+    ],
+
+    // Page
+    'System version information' => 'Информация о версии системы',
+    'Web application' => 'Веб-приложение',
+    'Short name' => 'Короткое название',
+    'Control panel' => 'Панель управления',
+    'Platform' => 'Платформа',
+    'Version' => 'Версия',
+    'Release date' => 'Дата выпуска',
+    'Details' => 'Подробнее',
+    'Edition' => 'Редакция',
+    'Administration panel' => 'Панель администрирования',
+    'Framework' => 'Фреймворк',
+    'Update packages' => 'Пакеты обновлений',
+    'Number of packages installed' => 'Количество установленных пакетов',
+    'View service packs' => 'Посмотреть пакеты обновлений'
+];
